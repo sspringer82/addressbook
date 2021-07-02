7922ca733ad24d0cacb3d92dbd5d6655eda023dc
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-07-02 11:52:26 +0200

    Aufgabe 6

---
+++ Aufgaben/Aufgabe6.md
@@ -0,0 +1,6 @@
+# Aufgabe6 - Lazy Loading und Router
+
+- Sorgt dafür, dass alle Komponenten im Routing lazy geladen werden
+  - Betrifft: List, Form, Detail
+- Verwendet hier ebenfalls einen Loading Indicator
+- Prüft die Ladereihenfolge und das Verhalten des Browsers
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
 import './App.css';
-import List from './List';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
-import Form from './Form';
-import Detail from './Detail';
+import React, { Suspense } from 'react';
+import { GridLoader } from 'react-spinners';
+
+// import Form from './Form';
+// import List from './List';
+// import Detail from './Detail';
+
+const Form = React.lazy(() => import('./Form'));
+const List = React.lazy(() => import('./List'));
+const Detail = React.lazy(() => import('./Detail'));
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/list">
-          <List />
-        </Route>
-        <Route path="/new">
-          <Form />
-        </Route>
-        <Route path="/edit/:id">
-          <Form />
-        </Route>
-        <Route path="/detail/:id">
-          <Detail />
-        </Route>
-        <Route path="/" exact>
-          <Redirect to="/list" />
-        </Route>
-      </Switch>
+      <Suspense fallback={<GridLoader />}>
+        <Switch>
+          <Route path="/list">
+            <List />
+          </Route>
+          <Route path="/new">
+            <Form />
+          </Route>
+          <Route path="/edit/:id">
+            <Form />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+          <Route path="/" exact>
+            <Redirect to="/list" />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
